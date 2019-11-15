d160a26f3a9c2e7783d29ab9dd98b6b82c9d7966
zhouyc <yuanchenzhouhcmy@gmail.com>
2019-11-15 15:08:40 +0800

    Add files via upload

--- Leetcode/DP/Medium/LongestIncreasingSubsequence.cc
+++ Leetcode/DP/Medium/LongestIncreasingSubsequence.cc
@@ -1,24 +1,39 @@
-#include <iostream>
+/**
+Given an unsorted array of integers, find the length of longest increasing subsequence.
 
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+Note:
+
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+**/
+
+
+#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), 1);
-        int res = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            res = max(res, dp[i]);
-        }
-        return res;
+    	vector<int> dp(nums.size(),1);
+
+    	int res = 0;
+    	for(int i=0; i<nums.size();i++){
+    		for(int j=0; j<i; j++){
+    			if(nums[i]>nums[j])
+    				dp[i] = max(dp[i],dp[j]+1);
+    		}
+    		res = max(dp[i],res);
+    	}
+    	return res;
     }
 };
 
@@ -26,7 +41,13 @@ public:
 int main(int argc, char const *argv[])
 {
 	Solution solu;
-	vector<int> arr ={10,9,2,5,3,4};//{-2,-1};//{10,9,2,5,3,7,101,18};
-	cout<<solu.lengthOfLIS(arr)<<endl;
+	vector<int> arr1={10,9,2};
+	vector<int> arr2={10,9,2,5,3};
+	vector<int> arr3={10,9,2,5,3,7,101,18};
+
+	cout<<solu.lengthOfLIS(arr1)<<endl;
+	cout<<solu.lengthOfLIS(arr2)<<endl;
+	cout<<solu.lengthOfLIS(arr3)<<endl;
+
 	return 0;
 }
