2c7668c913710bce77084a34c09f9464c1d38b06
zhouyc <yuanchenzhouhcmy@gmail.com>
2020-03-10 21:05:12 +0800

    Create 从尾到头打印链表.cc

---
+++ JZOF/从尾到头打印链表.cc
@@ -0,0 +1,82 @@
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
+/*
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> res;
+        
+        ListNode *pre_node,*next_node;
+        
+        pre_node = head;
+        next_node = pre_node;
+         if(head==NULL)
+            return res;
+
+        while(head->next){
+        	next_node = head->next;
+        	head->next = next_node->next;
+        	next_node->next = pre_node;
+        	pre_node = next_node;
+        }
+        while(pre_node){
+        	res.push_back(pre_node->val);
+        	pre_node = pre_node->next;
+        }
+
+        return res;
+    }
+};*/
+
+//first time
+/*
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> res;
+        while(head){
+        	res.push_back(head->val);
+        	head = head->next;
+        }
+
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
+*/
+
+
+
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> res;
+        
+        if(!head) return res;
+
+        ListNode *pre_node = nullptr;
+        ListNode *sec_node = nullptr;
+
+        while(head){
+        	sec_node = head->next;
+        	head->next = pre_node;
+        	pre_node = head;
+        	head = sec_node;
+        }
+        head = pre_node;
+        while(head){
+        	res.push_back(head->val);
+        	head = head->next;
+        }
+
+        return res;
+
+    }
+};
